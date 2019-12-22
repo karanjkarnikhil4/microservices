6ca0ef6a1a27653d21df43c3558fcabce86440c0
Nikhil <karanjkarnikhil4@gmail.com>
2019-12-22 10:44:47 +0530

    adding all the microservice related information

--- limits-service/src/main/java/com/nikhilkaranjkar/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/nikhilkaranjkar/microservices/limitsservice/LimitsConfigurationController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.nikhilkaranjkar.microservices.limitsservice.bean.LimitsConfiguration;
 
 @RestController
@@ -16,6 +17,17 @@ public class LimitsConfigurationController {
 	public LimitsConfiguration reteiveLimitsFromConfigurations()
 	{
 		return new LimitsConfiguration(configuraiton.getMaximum(),configuraiton.getMinimum());
+	}
+	
+	
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+	public LimitsConfiguration retrieveConfiguration() {
+		throw new RuntimeException("Not available");
+	}
+
+	public LimitsConfiguration fallbackRetrieveConfiguration() {
+		return new LimitsConfiguration(6666, 6);
 	}
 	
 	
